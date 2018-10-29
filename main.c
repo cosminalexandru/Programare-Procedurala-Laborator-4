fe332e0899f0e95e3180a466bde2ff248dd1b7d5
Cosmin Alex <silvian1995@yahoo.com>
2018-10-29 18:21:11 +0200

    Problem 1

--- main.c
+++ main.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
+/**
+ * Enum
+ */
+enum {
+    Encryption,
+    Decryption
+};
+
+/**
+ * Union
+ */
+typedef union {
+    int number : 16;
+    char v[2];
+} Fields;
+
+/**
+ *
+ * @param value - the element that will be changed
+ * @param type - the operation type - Encryption/Decryption
+ * @return new value obtained after Encryption/Decryption
+ */
+Fields swap_order(Fields value, int type) {
+    switch (type) {
+        case Encryption: {
+            char aux = value.v[0];
+            value.v[0] = value.v[1];
+            value.v[1] = aux;
+            return value;
+        }
+        case Decryption: {
+            char aux = value.v[1];
+            value.v[1] = value.v[0];
+            value.v[0] = aux;
+            return value;
+        }
+        default:
+            return value;
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    Fields var, returned_var;
+    int aux_number;
+    printf("Insert number");
+    scanf("%d", &aux_number);
+    var.number = aux_number;
+    returned_var = swap_order(var, Decryption);
+    printf("Encrypted number is:%d", returned_var.number);
+
 }
